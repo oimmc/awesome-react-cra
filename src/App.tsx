9bea01e374a9a54b8969d04f6a8b534af6095013
wangyj <wangyj@ycfin.com.cn>
2019-12-13 17:41:20 +0800

    chore: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import './assets/style/global.less'
 // import loadable from '@loadable/component'
 // import Loading from './Loading'
 
@@ -14,13 +15,6 @@ import React from 'react'
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-// Some folks find value in a centralized route config.
-// A route config is just data. React is great at mapping
-// data into components, and <Route> is a component.
-
-// Our route config is just an array of logical "routes"
-// with `path` and `component` props, ordered the same
-// way you'd do inside a `<Switch>`.
 const routes: any = [
 	{
 		path: '/sandwiches',
@@ -65,15 +59,11 @@ export default function RouteConfigExample() {
 	)
 }
 
-// A special wrapper for <Route> that knows how to
-// handle "sub"-routes by passing them in a `routes`
-// prop to the component it renders.
 function RouteWithSubRoutes(route: any) {
 	return (
 		<Route
 			path={route.path}
 			render={props => (
-				// pass the sub-routes down to keep nesting
 				<route.component {...props} routes={route.routes} />
 			)}
 		/>
@@ -113,33 +103,3 @@ function Bus() {
 function Cart() {
 	return <h3>Cart</h3>
 }
-
-// import React from 'react'
-// import logo from './logo.svg'
-// import './App.css'
-// import './assets/style/global.less'
-
-// const App: React.FC = () => {
-// 	return (
-// 		<div className="App">
-// 			<header className="App-header">
-// 				<img src={logo} className="App-logo" alt="logo" />
-// 				<p>
-// 					Edit
-// 					<code>src/App.tsx</code>
-// 					and save to reload.
-// 				</p>
-// 				<a
-// 					className="App-link"
-// 					href="https://reactjs.org"
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 				>
-// 					Learn React
-// 				</a>
-// 			</header>
-// 		</div>
-// 	)
-// }
-
-// export default App
